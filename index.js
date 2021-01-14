7fe0dfbbf9270df428b40a469c0c1d01e502790a
nevstas <Uf787euyYYwe>
2021-01-14 16:32:38 +0300

    Переделано на callback

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-var ads_success = 0;
-var ads_error = 0;
-var ads = [];
-
 function num2str(n, text_forms) {
     n = Math.abs(n) % 100; var n1 = n % 10;
     if (n > 10 && n < 20) { return text_forms[2]; }
@@ -10,11 +6,11 @@ function num2str(n, text_forms) {
     return text_forms[2];
 }
 
-function up() {
+function up(callback) {
 
-    ads_success = 0;
-    ads_error = 0;
-    ads = [];
+    var ads_success = 0;
+    var ads_error = 0;
+    var ads = [];
 
     $("li").each(function(index, element) {
         if ($(element).attr('rel')) {
@@ -38,23 +34,26 @@ function up() {
                 } else {
                     ads_error++;
                 }
-                result();
+                if (ads_success + ads_error == ads.length && typeof callback === 'function') {
+                    callback(ads_success, ads_error, ads.length);
+                }
             },
             error: function (xhr, status, thrownError) {
                 ads_error++;
-                result();
+                if (ads_success + ads_error == ads.length && typeof callback === 'function') {
+                    callback(ads_success, ads_error, ads.length);
+                }
             },
         });
     });
-
 }
 
-function result() {
-    if (ads_success + ads_error == ads.length) {
+function run() {
+    up(function (cb_success, cb_error, cb_length) {
         var date = new Date().toLocaleString();
-        console.log(date + ' Попытка поднять ' + ads.length + ' ' + num2str(ads.length, ['объявление', 'объявления', 'объявлений']) + ', успешно поднято ' + ads_success + ' ' + num2str(ads_success, ['объявление', 'объявления', 'объявлений']) + ', ждем 18 часов до следующего поднятия');
-    }
+        console.log(date + ' Попытка поднять ' + cb_length + ' ' + num2str(cb_length, ['объявление', 'объявления', 'объявлений']) + ', успешно поднято ' + cb_success + ' ' + num2str(cb_success, ['объявление', 'объявления', 'объявлений']) + ', ждем 18 часов до следующего поднятия');
+    });
 }
 
-up();
-setInterval(up, 64860000); //18 часов и 1 минута
+run();
+setInterval(run(), 64860000); //18 часов и 1 минута
